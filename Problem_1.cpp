3df9e489b8110d131a3f9a77e4a464e4cd4c8095
Arundhati Das <44167900+Arundhuti2000@users.noreply.github.com>
2020-09-06 13:19:40 +0530

    Find the word using trie

---
+++ Problem_1.cpp
@@ -0,0 +1,117 @@
+// C++ implementation of search and insert 
+// operations on Trie 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+const int ALPHABET_SIZE = 26; 
+
+// trie node 
+struct TrieNode 
+{ 
+	struct TrieNode *children[ALPHABET_SIZE]; 
+
+	// isEndOfWord is true if the node represents 
+	// end of a word 
+	bool isEndOfWord; 
+}; 
+
+// Returns new trie node (initialized to NULLs) 
+struct TrieNode *getNode(void) 
+{ 
+	struct TrieNode *pNode = new TrieNode; 
+
+	pNode->isEndOfWord = false; 
+
+	for (int i = 0; i < ALPHABET_SIZE; i++) 
+		pNode->children[i] = NULL; 
+
+	return pNode; 
+} 
+
+// If not present, inserts key into trie 
+// If the key is prefix of trie node, just 
+// marks leaf node 
+void insert(struct TrieNode *root, string key) 
+{ 
+	struct TrieNode *pCrawl = root; 
+
+	for (int i = 0; i < key.length(); i++) 
+	{ 
+		int index = key[i] - 'a'; 
+		if (!pCrawl->children[index]) 
+			pCrawl->children[index] = getNode(); 
+
+		pCrawl = pCrawl->children[index]; 
+	} 
+
+	// mark last node as leaf 
+	pCrawl->isEndOfWord = true; 
+} 
+
+// Returns true if key presents in trie, else 
+// false 
+bool search(struct TrieNode *root, string key) 
+{ 
+	struct TrieNode *pCrawl = root; 
+
+	for (int i = 0; i < key.length(); i++) 
+	{ 
+		int index = key[i] - 'a'; 
+		if (!pCrawl->children[index]) 
+			return false; 
+
+		pCrawl = pCrawl->children[index]; 
+	} 
+
+	return (pCrawl != NULL && pCrawl->isEndOfWord); 
+} 
+
+// Driver 
+int main() 
+{ 
+	// Input keys (use only 'a' through 'z' 
+	// and lower case) 
+	string myText;
+	//string temp="";
+	string dic[5000];
+	int i = 0;
+	string temp[5000];
+// Read from the text file
+	ifstream MyReadFile("EnglishDictionary.csv");
+	
+	while (getline (MyReadFile, myText)) {
+  	//temp=temp+myText+"\n";
+	  dic[i]=myText.substr(0,myText.find(","));
+	  temp[i]=myText.substr(myText.find(",") + 1);
+	  i++;
+	}
+
+	
+	
+	string keys[5000];
+	
+	for(int k = 0;k<5000;k++){
+		keys[k]=dic[k];
+	}
+					
+	string word2Search ="";
+	cout<<"Enter word to search : ";
+	cin>>word2Search;
+	int n = sizeof(keys)/sizeof(keys[0]); 
+
+	struct TrieNode *root = getNode(); 
+
+	// Construct trie 
+	for (int i = 0; i < n; i++) 
+		insert(root, keys[i]); 
+
+	// Search for different keys
+	
+	if(search(root,word2Search)){
+		cout <<word2Search<<" is present = "<< "Yes\n";
+	}else {
+		cout <<word2Search<<" is present = "<< "No\n";
+	}
+	return 0; 
+} 
+
---
+++ README.txt
@@ -0,0 +1,2 @@
+Download the EnglishDictionary File and save it into the same folder as the main cpp file.
+Compile and execute the cpp file.
